d7c30a7a1e84bfdd93a6dd991303f7746533c86d
oscar <oscaroceguera@gmail.com>
2016-11-24 23:36:26 -0700

    init

--- app/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'sharedStyles/styles.css'
-import { MainContainer } from 'containers'
-
-ReactDOM.render(
-  <MainContainer />,
-  document.getElementById('root')
-)
